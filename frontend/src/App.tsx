c54dccae2c9a40391bbce351f126d7abc724f6c8
Stefano <323267+futtetennista@users.noreply.github.com>
2024-11-06 10:47:10 +0000

    Chat persist history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Chat } from "@/components/Chat";
-import { Model, models } from "@/types";
+import { Model } from "@/types";
+import { v4 } from "uuid";
 
 const App: React.FC = () => {
-  const [defaultModel, setDefaultModel] = useState<Model>(models[0]);
+  console.log("App component rendered");
+  const [model, setModel] = useState<Model>("chatgpt");
+  const [chatId, setChatId] = useState<string>(v4());
+
+  useEffect(() => {
+    let storedChatId = sessionStorage.getItem("app.currentChatId");
+    if (!storedChatId) {
+      console.log("No chatId found in sessionStorage, generating a new one");
+      sessionStorage.setItem("app.currentChatId", chatId);
+      setChatId(chatId);
+    } else {
+      console.log("Found chatId in sessionStorage", storedChatId);
+      setChatId(storedChatId);
+    }
+  }, []);
 
   return (
     <div className="h-screen">
-      <div className="bg-gray-100 p-4">
-        <select
+      {/* <div className="bg-gray-100 p-4"> */}
+      {/* <select
           value={defaultModel}
           onChange={(e) => {
             const model = models.find((name) => name === e.target.value);
@@ -25,8 +40,8 @@ const App: React.FC = () => {
             </option>
           ))}
         </select>
-      </div>
-      <Chat defaultModel={defaultModel} />
+      </div> */}
+      {chatId && <Chat modelState={[model, setModel]} chatId={chatId} />}
     </div>
   );
 };
