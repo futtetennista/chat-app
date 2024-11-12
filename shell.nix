bed34b62f0edb4453e79322dec9d39d897d85040
futtetennista <futtetennista@pm.me>
2024-11-12 17:11:20 +0000

    Build AWS SAM derivation
    It's not immediately apparent from https://github.com/NixOS/nixpkgs/blob/2d2a9ddbe3f2c00747398f3dc9b05f7f2ebb0f53/pkgs/development/interpreters/python/mk-python-derivation.nix
    why this works.
    But it does. And `doCheck = false` does NOT.

--- shell.nix
+++ shell.nix
@@ -4,13 +4,15 @@
   pkgs ? import sources.nixpkgs { inherit system; config = {}; overlays = []; },
 }:
 
-pkgs.mkShell {
+pkgs.mkShellNoCC {
   packages = [
     pkgs.git
     pkgs.nodejs_20
     pkgs.pnpm
     pkgs.shellcheck
-    # pkgs.aws-sam-cli.override { doChecks = false; }
+    (pkgs.aws-sam-cli.overrideAttrs (oldAttrs: {
+      doInstallCheck = false;
+    }))
     # pkgs.husky
   ];
 }
