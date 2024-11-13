e5daa7ada83f4baecbdd679f46bf9c5b27727d92
Stefano <323267+futtetennista@users.noreply.github.com>
2024-11-13 15:29:00 +0000

    Upload playwright reports

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,35 +2,9 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-async function enableMocking() {
-  console.log("NODE_ENV", process.env.NODE_ENV);
-  if (process.env.NODE_ENV === "production") {
-    return undefined;
-  }
-
-  try {
-    const { internalHandlers: handlers } = await import("@chat-app/mocks");
-    const { setupWorker } = await import("msw/browser");
-    const worker = setupWorker(...handlers);
-    const res = await worker.start({
-      onUnhandledRequest: "warn",
-    });
-    return res;
-  } catch (error) {
-    console.error("Failed to import mocks", error);
-    return undefined;
-  }
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Element with id 'root' not found");
 }
 
-enableMocking()
-  .then(() => {
-    const container = document.getElementById("root");
-    if (!container) {
-      throw new Error("Element with id 'root' not found");
-    }
-
-    createRoot(container).render(<App />);
-  })
-  .catch((error: unknown) => {
-    console.error("Failed to enable mocking", error);
-  });
+createRoot(container).render(<App />);
