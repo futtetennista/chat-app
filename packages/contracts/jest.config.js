214dfd0a862ee9698feaa4d4ebfb599536a3cf4f
Stefano Dacchille <stefano.dacchille@synthace.com>
2024-11-19 17:21:14 +0000

    Add reportes to jest config

--- packages/contracts/jest.config.js
+++ packages/contracts/jest.config.js
@@ -1,5 +1,11 @@
 /* eslint-disable */
 module.exports = {
+  clearMocks: true,
+  resetMocks: true,
+  reporters:
+    process.env["CI"] === "true"
+      ? [["github-actions", { silent: false }], "summary"]
+      : undefined,
   transform: {
     // "^.+\\.(t|j)sx?$": "ts-jest",
     "^.+\\.(t|j)sx?$": "@swc/jest",
