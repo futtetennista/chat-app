3f7e9b457c5d406738b40fa62b789f87ab3c8337
Stefano <323267+futtetennista@users.noreply.github.com>
2024-11-20 15:19:36 +0000

    Use tsc instead of ts-node

--- packages/frontend-infrastructure/src/config.ts
+++ packages/frontend-infrastructure/src/config.ts
@@ -1,13 +1,20 @@
+import { pipe } from "fp-ts/lib/function";
 import * as D from "io-ts/Decoder";
 
-export const Config = D.struct({
-  frontend: D.struct({
-    bucket: D.string,
+export const Config = pipe(
+  D.struct({
+    frontend: D.struct({
+      bucket: D.string,
+    }),
+    accessKey: D.string,
+    region: D.string,
+    secretKey: D.string,
   }),
-  accessKey: D.string,
-  profile: D.string,
-  region: D.string,
-  secretKey: D.string,
-});
+  D.intersect(
+    D.partial({
+      profile: D.string,
+    }),
+  ),
+);
 
 export type Config = D.TypeOf<typeof Config>;
