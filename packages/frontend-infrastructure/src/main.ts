19c671793dceb974299908db5c65f70ad898f7ca
futtetennista <futtetennista@pm.me>
2024-11-20 15:19:36 +0000

    Use tsc instead of ts-node

--- packages/frontend-infrastructure/src/main.ts
+++ packages/frontend-infrastructure/src/main.ts
@@ -1,30 +1,40 @@
+#!/usr/bin/env -S node --import @swc-node/register/esm-register
+
 import { App } from "cdktf";
 import * as D from "io-ts/Decoder";
 
 import { Config } from "@/config";
 import { Frontend } from "@/stacks/frontend";
 
-const config: Config = (function parseConfig() {
-  if (!process.env.FRONTEND_STACK_JSON_CONFIG) {
-    console.error("FRONTEND_STACK_JSON_CONFIG is not defined");
+function parseConfig(): Config {
+  if (!process.env.FRONTEND_INFRASTRUCTURE_JSON_CONFIG) {
+    console.error("FRONTEND_INFRASTRUCTURE_JSON_CONFIG is not defined");
     process.exit(1);
   }
-
   try {
-    const json = JSON.parse(process.env.FRONTEND_STACK_JSON_CONFIG) as Config;
+    const json = JSON.parse(
+      process.env.FRONTEND_INFRASTRUCTURE_JSON_CONFIG,
+    ) as Config;
+
     const configOrError = Config.decode(json);
     if (configOrError._tag === "Left") {
       console.error(D.draw(configOrError.left));
       process.exit(1);
     }
 
+    console.log("✅ Config parsed successfully");
     return configOrError.right;
   } catch (error) {
     console.error("Could not parse config", error);
     process.exit(1);
   }
-})();
+}
 
-const app = new App();
-new Frontend(app, "frontend", config);
-app.synth();
+function main() {
+  const config: Config = parseConfig();
+  const app = new App();
+  new Frontend(app, "frontend", config);
+  app.synth();
+}
+
+main();
